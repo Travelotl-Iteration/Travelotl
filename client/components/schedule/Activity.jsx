8bf9fc79770e8ae7eae67310c3d50300ef6a1849
Will Suto <will.suto@gmail.com>
2024-05-01 10:23:02 -0700

    activity loader

--- client/components/schedule/Activity.jsx
+++ client/components/schedule/Activity.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ItemTypes } from './Constants.js';
 import { useDrag, useDrop } from 'react-dnd';
 import { useDispatch, useSelector } from 'react-redux';
 import { itineraryActivityReplaced, itineraryRearranged } from '../../reducers/itineraryReducer.js';
+import MiniLoader from '../MiniLoader.jsx';
 
 const Activity = ({ activity, description, address, day, index, onDrop }) => {
+  const [loading, setLoading] = useState(false);
   const itinerary = useSelector(state => state.itinerary.itinerary)
-
   const dispatch = useDispatch();
 
   const [{isDragging}, drag] = useDrag(() => ({
@@ -31,37 +32,40 @@ const Activity = ({ activity, description, address, day, index, onDrop }) => {
   const handleDeleteClick = async () => {
     // make a request to back end to query new activity
     try {
+      setLoading(true);
       const response = await fetch('/api/trip/newActivity', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({itinerary, activity, description, address})
       });
       const activityObj = await response.json();
+      setLoading(false);
       dispatch(itineraryActivityReplaced({ activityObj, day, index }));
     } catch (error) { console.log('Error in fetch request to newActivity :', error) };
-
   };
 
   return (
-    <div className='activity' ref={drag} style={{opacity: isDragging ? 0.5 : 1, cursor: 'move'}}>
-      <div ref={drop} style={{ backgroundColor: isOver ? 'grey' : 'transparent', height: 'auto', marginTop: '10px', display: 'flex', flexDirection: 'column' }}>
-        <button onClick={handleDeleteClick}>X</button>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h3 style={{ margin: '0', marginRight: '5px' }}>Activity:</h3>
-          <p style={{ margin: '0' }}>{activity}</p>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h3 style={{ margin: '0', marginRight: '5px' }}>Description:</h3>
-          <p style={{ margin: '0' }}>{description}</p>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h3 style={{ margin: '0', marginRight: '5px' }}>Address:</h3>
-          <p style={{ margin: '0' }}>{address}</p>
-        </div>
+    <>
+      <div className='activity' ref={drag} style={{opacity: isDragging ? 0.5 : 1, cursor: 'move'}}>
+        {loading ? (<div className='activityLoader'><MiniLoader /></div>) :
+        (<div ref={drop} style={{ backgroundColor: isOver ? 'grey' : 'transparent', height: 'auto', marginTop: '10px', display: 'flex', flexDirection: 'column' }}>
+          <button onClick={handleDeleteClick}>X</button>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <h3 style={{ margin: '0', marginRight: '5px' }}>Activity:</h3>
+            <p style={{ margin: '0' }}>{activity}</p>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <h3 style={{ margin: '0', marginRight: '5px' }}>Description:</h3>
+            <p style={{ margin: '0' }}>{description}</p>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <h3 style={{ margin: '0', marginRight: '5px' }}>Address:</h3>
+            <p style={{ margin: '0' }}>{address}</p>
+          </div>
+        </div>)}
       </div>
-    </div>
-  );
-  
+    </>
+  )
   
 };
 
