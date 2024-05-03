64150b925c72b6cf88796283c07520127fa2a4e6
Will Suto <will.suto@gmail.com>
2024-05-02 22:28:16 -0700

    rendering restaurant markers on map

--- client/components/mapView.jsx
+++ client/components/mapView.jsx
@@ -1,139 +1,68 @@
-import React from 'react';
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+import React, {useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { Map, APIProvider, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 
-function MyMapComponent() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyD-QBFaA4K7IAT-N4hiOsUbmrt2CNSG9TA"
-  });
+const apiKey = 'AIzaSyBIrtoIQCvA4M7fKqg0UmZM5AZnSsJwVhk';
+
+function MapUI() {
+  const [markers, setMarkers] = useState([]);
+  const [center, setCenter] = useState()
+  const restaurants = useSelector(state => state.itinerary.restaurants);
+
+  useEffect(() => {
+    // Make an array of restaurant addresses
+    const addressArray = [];
+    restaurants.forEach(restaurant => {
+      addressArray.push(`${restaurant.address},${restaurant.zipcode}`)
+    });
+    // Declare async function to be invoked at end of useEffect hook
+    const getCoords = async (addressArray) => {
+      // Get the coordinates of all the restaurants
+      const markerCoords = [];
+      for (const address of addressArray) {
+        try { 
+          const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
+          const response = await fetch(geocodingUrl);
+          const responseObj = await response.json();
+          const coords = responseObj.results[0].geometry.location;
+          markerCoords.push(coords);
+        } catch (error) { console.log('error geocoding restaurant coordinates: ', error) };
+      };
+      // Set map center to coords of first restaurant
+      setCenter(markerCoords[0]);
+      // Create a marker for each restaurant
+      const markers = [];
+      markerCoords.forEach(coords => {
+        markers.push(<AdvancedMarker
+          position={coords}
+          title={'AdvancedMarker with customized pin.'}>
+          <Pin
+            background={'#22ccff'}
+            borderColor={'#1e89a1'}
+            glyphColor={'#0f677a'}></Pin>
+        </AdvancedMarker>)
+      })
+      setMarkers(markers);
+    };
+    // Invoke async function
+    getCoords(addressArray);
+  }, [])
 
   return (
-    <div>
-      {!isLoaded && <div>Loading...</div>}
-      {isLoaded && (
-        <GoogleMap className="map"
-          mapContainerStyle={{ width: '100vw', height: '100vh' }}
-          center={{ lat: 40.747761, lng: -73.993362 }}
-          zoom={4}
-          mapId={"559d048485f2b264"}
+    <APIProvider apiKey={apiKey}>
+      {/* Conditionally render the map and restaurant markers once the center has been determined */}
+      {center && <Map
+        style={{width: '100vw', height: '100vh'}}
+        defaultCenter={center}
+        defaultZoom={15}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+        mapId={'d96917bb7cbafeb6'}
         >
-          <Marker
-            position={{ lat: 61.2176, lng: -149.8997 }}
-            icon={{
-              url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            }}
-          />
-        </GoogleMap>
-      )}
-    </div>
+        {markers}
+      </Map>}
+    </APIProvider>
   );
 }
 
-export default MyMapComponent;
-
-// import React, { useEffect, useState, useRef } from 'react';
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
-
-// function MyMapComponent({ addresses }) {
-//   const [mapLoaded, setMapLoaded] = useState(false);
-//   const [map, setMap] = useState(null);
-//   const [searchBox, setSearchBox] = useState(null);
-//   const markerRef = useRef(null);
-
-//   useEffect(() => {
-//     const handleMapLoad = () => {
-//       setMapLoaded(true);
-//       const input = document.getElementById("search-input");
-//       const autoComplete = new window.google.maps.places.Autocomplete(input);
-//       autoComplete.addListener("place_changed", onPlaceChanged);
-//       setSearchBox(autoComplete);
-//     };
-
-//     window.addEventListener('load', handleMapLoad);
-
-//     return () => {
-//       window.removeEventListener('load', handleMapLoad);
-//     };
-//   }, []);
-
-//   const onLoad = (map) => {
-//     setMap(map);
-//     setMapLoaded(true);
-//     console.log('on loaded:', map);
-//   };
-
-
-//   const gps_start_LAT = 40.712800;
-//   const gps_start_LONG = -74.006000;
-
-//   return (
-//     <div style={{ position: 'relative' }}>
-//       <LoadScript googleMapsApiKey="AIzaSyD-QBFaA4K7IAT-N4hiOsUbmrt2CNSG9TA">
-//         <GoogleMap
-//           mapContainerStyle={{ width: '100%', height: '100vh' }}
-//           center={{ lat: gps_start_LAT, lng: gps_start_LONG }}
-//           zoom={8}
-//           onLoad={onLoad}
-//           mapId={'e6f7185b209c673a'}
-//         >
-//           {LoadScript && (
-//             <Marker 
-//             position={{ lat: 40.747761, lng: -73.993362 }} 
-//             icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-//             />
-//           )}
-//         </GoogleMap>
-//       </LoadScript>
-//       <div style={{ position: 'absolute', top: '100px', left: '10px', zIndex: '1' }} >
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default MyMapComponent;
-// import React, { FunctionComponent } from 'react';
-// import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
-// import React from 'react';
-// import {APIProvider, Map, InfoWindow} from '@vis.gl/react-google-maps';
-
-
-// const MyMapComponent = () => {
-//   const [infoWindowOpen, setInfoWindowOpen] = useState(false);
-//   const [selectedMarker, setSelectedMarker] = useState(null);
-
-//   const handleMarkerClick = (marker) => {
-//     setSelectedMarker(marker);
-//     setInfoWindowOpen(true);
-//   };
-
-//   const handleInfoWindowClose = () => {
-//     setInfoWindowOpen(false);
-//     setSelectedMarker(null);
-//   };
-
-//   return (
-//     <APIProvider apiKey={'AIzaSyD-QBFaA4K7IAT-N4hiOsUbmrt2CNSG9TA'}>
-//       <LoadScript>
-//         <Map zoom={12} center={{ lat: 53.54992, lng: 10.00678 }}  mapId={'e6f7185b209c673a'}>
-//           <Marker
-//             position={{ lat: 53.54992, lng: 10.00678 }}
-//             onClick={() => handleMarkerClick('marker1')}
-//           />
-//           {infoWindowOpen && selectedMarker && (
-//             <InfoWindow
-//               position={selectedMarker.position}
-//               onCloseClick={handleInfoWindowClose}
-//             >
-//               <div>
-//                 <h2>Info Window Content</h2>
-//                 <p>This is the content of the Info Window.</p>
-//               </div>
-//             </InfoWindow>
-//           )}
-//         </Map>
-//       </LoadScript>
-//     </APIProvider>
-//   );
-// };
-
-// export default MyMapComponent;
+export default MapUI;
