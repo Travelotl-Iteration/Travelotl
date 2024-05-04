e2e86dc1fc092623bc6ff30f79bf1e75fdeee928
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-05-04 10:46:19 -0400

    styling, infomarker window not rendering

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,52 +1,7 @@
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
 import palmLogo from '../assets/palm_logo.jpeg';
-// const Main = () => {
-//   const navigate = useNavigate();
-  
-//   function GO(){
-//       navigate('/form')
-//   }
-  
-//   return (
-//     <div>
-//       <Header />
-//       <p>Let us plan the trip of your dreams...</p>
-//       <button id='start' onClick={GO}>Click here to get started... </button>
-//     </div>
-//   );
-// };
 
-// export default Main;
-// export default function Main() {
-//     const navigate = useNavigate();
-  
-//   function GO(){
-//       navigate('/form')
-//   }
-  
-//   return (
-//     <>
-//     <Header />
-//     <section className="relative isolate overflow-hidden bg-white px-4 py-20 sm:py-20 lg:px-4">
-//       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-//       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-//       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-//       <blockquote className=" text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-//           <p>
-//               Let us plan the trip of your dreams...
-//             </p>
-//             </blockquote>
-//           <button id='start' onClick={GO} className="mx-auto text-center">
-//         <img className="mx-auto " src={palmLogo} alt="palm tree version of logo" style={{ borderRadius: '50%' }} />
-//         <figure className="mt-10">
-//         </figure>
-//      </button> 
-//       </div>
-//     </section>
-//     </>
-//   )
-// }
 export default function Main() {
   const navigate = useNavigate();
 
@@ -57,7 +12,7 @@ export default function Main() {
   return (
       <>
           <Header />
-          <section className="flex items-center justify-center h-screen bg-peach">
+          <section className="flex items-center justify-center h-screen bg-peach" >
               <div>
                   <blockquote className="text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9 text-center">
                       <p>Let us plan the trip of your dreams...</p>
--- client/components/map/HotelMarker.jsx
+++ client/components/map/HotelMarker.jsx
@@ -1,54 +1,77 @@
-// import { AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
-// import HotelModal from '../HotelModal.jsx';
-
-// const HotelMarker = ({coords, hotelName}) => {
-//   function handleHotelMarkerClick () {
-
-//   }
-//   return (
-//     <AdvancedMarker
-//       position={coords}
-//       title={hotelName}>
-//       <Pin
-//         background={'#ff4d4d'}
-//         borderColor={'#cc3333'}
-//         glyphColor={'#990000'}
-//         onClick={handleHotelMarkerClick}></Pin>
-//     </AdvancedMarker>
-//   );
-// }
-
-// export default HotelMarker;
-import React, { useState } from 'react';
 import { AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 import HotelModal from '../HotelModal.jsx';
 
-const HotelMarker = ({ coords, hotelName, hotelData }) => {
-  const [showModal, setShowModal] = useState(false);
+const HotelMarker = ({coords, hotelName}) => {
+  const [markerRef, marker] = useAdvancedMarkerRef();
 
-  function handleHotelMarkerClick() {
-    // setShowModal(true);
-    console.log('clicked')
-  }
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+
+
+  // clicking the marker will toggle the infowindow
+  const handleMarkerClick = useCallback(() =>
+    setInfoWindowShown(isShown => !isShown)
+  );
+
+  // if the maps api closes the infowindow, we have to synchronize our state
+  const handleClose = useCallback(() => setInfoWindowShown(false));
 
   return (
-    <>
-      <AdvancedMarker position={coords} title={hotelName}   onClick={handleHotelMarkerClick}>
-        <Pin
-          background={'#ff4d4d'}
-          borderColor={'#cc3333'}
-          glyphColor={'#990000'}
-        />
-      </AdvancedMarker>
-      {showModal && (
-        <HotelModal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          hotelData={hotelData}
-        />
+    <AdvancedMarker
+      position={coords}
+      title={hotelName}
+      onClick={handleMarkerClick}
+      ref={markerRef}
+      >
+      <Pin
+        background={'#ff4d4d'}
+        borderColor={'#cc3333'}
+        glyphColor={'#990000'}
+       >
+  {infoWindowShown && (
+        <InfoWindow anchor={marker} onClose={handleClose}>
+          <h2>InfoWindow content!</h2>
+          <p>Some arbitrary html to be rendered into the InfoWindow.</p>
+        </InfoWindow>
       )}
-    </>
+
+       </Pin>
+
+    
+    </AdvancedMarker>
   );
-};
+}
 
 export default HotelMarker;
+// import React, { useState } from 'react';
+// import { AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+// import HotelModal from '../HotelModal.jsx';
+
+// const HotelMarker = ({ coords, hotelName, hotelData }) => {
+//   const [showModal, setShowModal] = useState(false);
+
+//   function handleHotelMarkerClick() {
+//     // setShowModal(true);
+//     console.log('clicked')
+//   }
+
+//   return (
+//     <>
+//       <AdvancedMarker position={coords} title={hotelName}   onClick={handleHotelMarkerClick}>
+//         <Pin
+//           background={'#ff4d4d'}
+//           borderColor={'#cc3333'}
+//           glyphColor={'#990000'}
+//         />
+//       </AdvancedMarker>
+//       {showModal && (
+//         <HotelModal
+//           showModal={showModal}
+//           setShowModal={setShowModal}
+//           hotelData={hotelData}
+//         />
+//       )}
+//     </>
+//   );
+// };
+
+// export default HotelMarker;
